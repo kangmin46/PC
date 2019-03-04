c756790a943d4892a8dbad13e67e28e6af778921
kangmin <kangmin789@naver.com>
2019-03-04 18:55:31 +0900

    코드길이 대량으로 줄이고 Toolbar1 , Toolbar2삭제

--- src/com/company/ServerView.java
+++ src/com/company/ServerView.java
@@ -11,7 +11,6 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Vector;
 import javax.swing.*;
 import javax.swing.table.*;
 
@@ -22,6 +21,8 @@ public class ServerView extends JFrame {
 
     static List<JButton> buttonList = new ArrayList<>();
     static ServerController serverController = new ServerController();
+    static String[] menuBarString = {"카운터설정","손님PC설정","인수인계/운영관리","요금/상품설정","기타관리",
+            "ASTsoft","커뮤니티","도움말"};
 
     static TableModel rowtableModel = new AbstractTableModel() {
         String headerData[] = {"코드입력","PC번호","고객이름","고객ID"};
@@ -41,17 +42,12 @@ public class ServerView extends JFrame {
         }
 
         public Object getValueAt(int row, int col) {
-            if(col==0) {
-                return headerData[row];
-            }
-            return columnData[row];
+            if(col==0) return headerData[row];
+            else return columnData[row];
         }
 
         public void setValueAt(Object aValue, int row, int col){
-            if(col==0){
-
-            }
-            else{
+            if(col != 0 ){
                 columnData[row]=String.valueOf(aValue);
                 fireTableCellUpdated(row, col);
             }
@@ -66,7 +62,7 @@ public class ServerView extends JFrame {
                 first = false;
                 return;
             }
-            tc.setMinWidth(79);
+            tc.setMinWidth(98);
             super.addColumn(tc);
         }
     };
@@ -76,7 +72,7 @@ public class ServerView extends JFrame {
 
         public void addColumn(TableColumn tc) {
             if (first) {
-                tc.setMaxWidth(79);
+                tc.setMaxWidth(98);
                 super.addColumn(tc);
                 first = false;
             }
@@ -86,7 +82,7 @@ public class ServerView extends JFrame {
     static TableModel tableModel = new DefaultTableModel() {
         String header[] = {"선불시간","이용한시간","남은시간"};
         String column[] = {"","",""};
-        Vector cache;
+
         @Override
         public int getRowCount() {
             return 2;
@@ -137,10 +133,8 @@ public class ServerView extends JFrame {
         }
 
         public Object getValueAt(int row, int col) {
-            if(col==0) {
-                return headerData[row];
-            }
-            return columnData[row];
+            if(col==0) return headerData[row];
+            else return columnData[row];
         }
     };
 
@@ -152,7 +146,7 @@ public class ServerView extends JFrame {
                 first = false;
                 return;
             }
-            tc.setMinWidth(78);
+            tc.setMinWidth(98);
             super.addColumn(tc);
         }
     };
@@ -162,13 +156,12 @@ public class ServerView extends JFrame {
 
         public void addColumn(TableColumn tc) {
             if (first) {
-                tc.setMaxWidth(78);
+                tc.setMaxWidth(98);
                 super.addColumn(tc);
                 first = false;
             }
         }
     };
-
 
 
     public ServerView() {
@@ -180,137 +173,30 @@ public class ServerView extends JFrame {
         menuBar1 = new JMenuBar();
         List<JMenu> menuList =new ArrayList<>();
 
-
-
         for(int i=0;i<8;i++){
             menuList.add(new JMenu());
         }
-        toolBar1 = new JToolBar();
-        button3 = new JButton();
-        button4 = new JButton();
-        button5 = new JButton();
-        button6 = new JButton();
-        button7 = new JButton();
-        button8 = new JButton();
-        button9 = new JButton();
+
         panel1 = new JPanel();
-        button10 = new JButton();
-        button11 = new JButton();
-        button12 = new JButton();
         panel2 = new JPanel();
         panel3 = new JPanel();
-        toolBar2 = new JToolBar();
-        button1 = new JButton();
-        button2 = new JButton();
         panel4 = new JPanel();
         panel5 = new JPanel();
-        scrollPane3 = new JScrollPane();
-        table3 = new JTable();
         panel6 = new JPanel();
-        scrollPane4 = new JScrollPane();
-        table4 = new JTable();
         panel7 = new JPanel();
-        scrollPane5 = new JScrollPane();
-        table5 = new JTable();
-
-
 //오른쪽꺼
 
         //======== this ========
         Container contentPane = getContentPane();
         contentPane.setLayout(new BorderLayout());
         //======== menuBar1 ========
-        {
-            JMenu menu1=menuList.get(0);
-            //======== menu1 ========
-            {
-                menu1.setText("\uce74\uc6b4\ud130\uc124\uc815");
-            }
-            menuBar1.add(menu1);
 
-            //======== menu2 ========
-            JMenu menu2=menuList.get(1);
-            {
-                menu2.setText("\uc190\ub2d8PC\uc124\uc815");
+            for(int i=0;i<8;i++){
+                menuList.get(i).setText(menuBarString[i]);
+                menuBar1.add(menuList.get(i));
             }
-            menuBar1.add(menu2);
 
-            //======== menu3 ========
-            JMenu menu3=menuList.get(2);
-            {
-                menu3.setText("\uc778\uc218\uc778\uacc4/\uc6b4\uc601\uad00\ub9ac");
-            }
-            menuBar1.add(menu3);
-
-            //======== menu4 ========
-            JMenu menu4=menuList.get(3);
-            {
-                menu4.setText("\uc694\uae08/\uc0c1\ud488\uc124\uc815");
-            }
-            menuBar1.add(menu4);
-
-            //======== menu5 ========
-            JMenu menu5=menuList.get(4);
-            {
-                menu5.setText("\uae30\ud0c0 \uad00\ub9ac");
-            }
-            menuBar1.add(menu5);
-
-            //======== menu6 ========
-            JMenu menu6=menuList.get(5);
-            {
-                menu6.setText("ASTsoft");
-            }
-            menuBar1.add(menu6);
-
-            //======== menu7 ========
-            JMenu menu7=menuList.get(6);
-            {
-                menu7.setText("\ucee4\ubba4\ub2c8\ud2f0");
-            }
-            menuBar1.add(menu7);
-
-            //======== menu8 ========
-            JMenu menu8=menuList.get(7);
-            {
-                menu8.setText("\ub3c4\uc6c0\ub9d0");
-            }
-            menuBar1.add(menu8);
-        }
         setJMenuBar(menuBar1);
-
-        //======== toolBar1 ========
-        {
-
-            //---- button3 ----
-            button3.setText("text");
-            toolBar1.add(button3);
-
-            //---- button4 ----
-            button4.setText("text");
-            toolBar1.add(button4);
-
-            //---- button5 ----
-            button5.setText("text");
-            toolBar1.add(button5);
-
-            //---- button6 ----
-            button6.setText("text");
-            toolBar1.add(button6);
-
-            //---- button7 ----
-            button7.setText("text");
-            toolBar1.add(button7);
-
-            //---- button8 ----
-            button8.setText("text");
-            toolBar1.add(button8);
-
-            //---- button9 ----
-            button9.setText("text");
-            toolBar1.add(button9);
-        }
-        contentPane.add(toolBar1, BorderLayout.NORTH);
 
         //======== panel1 ========
         {
@@ -375,7 +261,7 @@ public class ServerView extends JFrame {
             buttonList.get(i).addActionListener(new ActionListener() {
                 @Override
                 public void actionPerformed(ActionEvent e) {
-                    System.out.println(idx+"버튼 클릭");
+
                     for (int j = 0; j<tableModel.getRowCount()+1; j++) {
                         tableModel.setValueAt("", j, 1);
                         rowtableModel.setValueAt("",j,1);
@@ -393,38 +279,18 @@ public class ServerView extends JFrame {
             panel2.setLayout(new BoxLayout(panel2, BoxLayout.X_AXIS));
 
             //======== panel3 ========
-            {
-                panel3.setLayout(new BoxLayout(panel3, BoxLayout.Y_AXIS));
-
-                //======== toolBar2 ========
-                {
-                    toolBar2.setOrientation(SwingConstants.VERTICAL);
-
-                    //---- button1 ----
-                    button1.setText("text");
-                    toolBar2.add(button1);
-
-                    //---- button2 ----
-                    button2.setText("text");
-                    toolBar2.add(button2);
-                }
-                panel3.add(toolBar2);
-            }
+            panel3.setLayout(new BoxLayout(panel3, BoxLayout.Y_AXIS));
             panel2.add(panel3);
 
             //======== panel4 ========
             {
                 panel4.setLayout(new BoxLayout(panel4, BoxLayout.Y_AXIS));
                 panel4.setPreferredSize(new Dimension(200, 600));
-
                 //======== panel5 ========
                 {
                     panel5.setLayout(new BoxLayout(panel5, BoxLayout.Y_AXIS));
                     panel5.setPreferredSize(new Dimension(100, 200));
-                    //======== scrollPane3 ========
-                    {
-                        scrollPane.setViewportView(firstTable);
-                    }
+                    scrollPane.setViewportView(firstTable);
                     panel5.add(scrollPane);
                 }
                 panel4.add(panel5);
@@ -433,11 +299,8 @@ public class ServerView extends JFrame {
                 {
                     panel6.setLayout(new BoxLayout(panel6, BoxLayout.Y_AXIS));
                     panel6.setPreferredSize(new Dimension(100, 60));
-
                     //======== scrollPane4 ========
-                    {
-                        scrollPane1.setViewportView(secondTable);
-                    }
+                    scrollPane1.setViewportView(secondTable);
                     panel6.add(scrollPane1);
                 }
                 panel4.add(panel6);
@@ -446,11 +309,8 @@ public class ServerView extends JFrame {
                 {
                     panel7.setLayout(new BoxLayout(panel7, BoxLayout.Y_AXIS));
                     panel7.setPreferredSize(new Dimension(100, 580));
-
                     //======== scrollPane5 ========
-                    {
-                        scrollPane2.setViewportView(thirdTable);
-                    }
+                    scrollPane2.setViewportView(thirdTable);
                     panel7.add(scrollPane2);
                 }
                 panel4.add(panel7);
@@ -471,7 +331,6 @@ public class ServerView extends JFrame {
             @Override
             public void windowClosing(WindowEvent e) {
                 // Dispose the window after the close button is clicked.
-                System.out.println("close 눌렀음");
                 serverController.ServerClose();
                 System.exit(0);
             }
@@ -506,33 +365,13 @@ public class ServerView extends JFrame {
     // JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
     // Generated using JFormDesigner Evaluation license - Kim kang min
     private JMenuBar menuBar1;
-
-    private JToolBar toolBar1;
-    private JButton button3;
-    private JButton button4;
-    private JButton button5;
-    private JButton button6;
-    private JButton button7;
-    private JButton button8;
-    private JButton button9;
     private JPanel panel1;
-    private JButton button10;
-    private JButton button11;
-    private JButton button12;
     private JPanel panel2;
     private JPanel panel3;
-    private JToolBar toolBar2;
-    private JButton button1;
-    private JButton button2;
     private JPanel panel4;
     private JPanel panel5;
-    private JScrollPane scrollPane3;
-    private JTable table3;
     private JPanel panel6;
-    private JScrollPane scrollPane4;
-    private JTable table4;
     private JPanel panel7;
-    private JScrollPane scrollPane5;
-    private JTable table5;
+
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
